6fcd777e5f2689dc944c373a12ae970eab83e17c
AnnSyh <ann.syharevskaya@gmail.com>
2022-10-17 16:15:29 +0300

    meduza-auth

---
+++ praktikum-cryptoducks-source-master-final/src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './NavBar.jsx';
+import CardList from './CardList.jsx';
+
+function Cards () {
+  return (
+    <>
+      <NavBar />
+      <CardList />
+    </>
+  )
+}
+
+export default Cards;
