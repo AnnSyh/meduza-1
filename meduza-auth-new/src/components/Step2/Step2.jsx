9128a2cc43e7d019d8f78f126502f4307d70105e
AnnSyh <ann.syharevskaya@gmail.com>
2022-10-24 10:38:40 +0300

    fix: radio button in Step2-age

--- meduza-auth-new/src/components/Step2/Step2.jsx
+++ meduza-auth-new/src/components/Step2/Step2.jsx
@@ -9,20 +9,8 @@ import '../scss/style.css';
 
 import Container from '@material-ui/core/Container';
 
-// you can use React.forwardRef to pass the ref too
-const Select = React.forwardRef(({ onChange, name, label }, ref) => (
-  <>
-    <label>{label}</label>
-    <select name={name} ref={ref} onChange={onChange}>
-      <option value="age-1">16-24</option>
-      <option value="age-2">25-34</option>
-      <option value="age-3">24-44</option>
-      <option value="age-4">45 +</option>
-    </select>
-  </>
-));
-
 const Step2 = (props) => {
+  // форма
   const {
     register,
     handleSubmit,
@@ -36,37 +24,48 @@ const Step2 = (props) => {
   const onSubmit = (data) => {
     console.log('data = ', data);
     actions.updateAction(data);
-    props.history.push("./Step3");
-    // props.history.push("./Result");
+    // props.history.push("./Step3");
+    props.history.push("./Result");
     reset();
   };
+
+  const CheckboxRadio = (props) => {
+    return (
+      <div
+        className="form_radio_btn"
+      >
+        <input
+          {...register('age', { required: true })}
+          type="radio"
+          name="age"
+          value={props.id}
+          id={props.id}
+        />
+        <label htmlFor={props.id}>
+          {props.id}
+        </label>
+      </div>
+    )
+  }
 
 
   return (
     <Container className='container__form-img container__form-img--right'>
-
       <h1>
         That’s a beautiful name, {state.data.firstName}!<br />
         I am [GUIDE]. How old are you?
       </h1>
       <div className='form-img'>
-        <div className='step2-buttons'>
-          <form
-            data-form='age-1'
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <Select label="Age" {...register("Age")} />
-
-            <button
-              className='beautiful-button'
-              type="submit"
-            >
-             submit
-            </button>
-          </form>
-
-        </div>
-
+        <form
+          onChange={handleSubmit(onSubmit)}
+        >
+          <div className='age-checks'>
+            <CheckboxRadio id='1' />
+            <CheckboxRadio id='2' />
+            <CheckboxRadio id='3' />
+            <CheckboxRadio id='4' />
+          </div>
+        </form>
       </div>
     </Container>
   );
